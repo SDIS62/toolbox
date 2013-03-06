f4716ccf8fcc5c9588568f04c8b47ce54d7bd3ca
Kévin DUBUC <kevindubuc62@gmail.com>
2013-03-06 17:31:35 +0100

    add action helper to get configuration values

---
+++ library/SDIS62/Controller/Helper/GetConfigurationValues.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * SDIS 62
+ *
+ * @category   SDIS62
+ * @package    SDIS62_Controller_Helper_GetConfigurationValues
+ */
+ 
+ /**
+ * Class for GetConfigurationValues Action Helper instance.
+ *
+ * @category   SDIS62
+ * @package    SDIS62_Controller_Helper_GetConfigurationValues
+ */
+class SDIS62_Controller_Helper_GetConfigurationValues extends Zend_Controller_Action_Helper_Abstract
+{
+
+    /**
+     * Get the configuration values based on the key param
+     *
+     * @param string $config_file Path to the configuration file
+     * @param string $configuration_parameters_key optional key to load in configuration file
+     * @return array
+     */
+    public function direct($config_file, $configuration_parameters_key = null)
+    {
+        // check if config_file exists
+        if(!file_exists($config_file))
+        {
+            throw new Zend_Exception("Configuration file does not exists (" . $config_file . ")");
+        }
+        
+        // retrieve the configuration from config_file
+        $config = new Zend_Config_Ini($this->config_file, APPLICATION_ENV);
+        
+        // get the key based configuration values
+        $key_configuration_values = $configuration_parameters_key == null ? $secret_config : $secret_config->$configuration_parameters_key;
+        
+        // Check if key_configuration_values is an array
+        if(!is_array($key_configuration_values->toArray()))
+        {
+            throw new Zend_Exception("Configuration parameters is not an array.");
+        }
+        
+        return $key_configuration_values->toArray();
+    }
+}
--- library/SDIS62/Oauth/Consumer/Controller/Abstract.php
+++ library/SDIS62/Oauth/Consumer/Controller/Abstract.php
@@ -53,25 +53,12 @@ abstract class SDIS62_Oauth_Consumer_Controller_Abstract extends Zend_Controller
         {
             $this->config_file = APPLICATION_PATH . DIRECTORY_SEPARATOR . "configs" . DIRECTORY_SEPARATOR . "secret.ini";
         }
-    
-        // check if config_file exists
-        if(!file_exists($this->config_file))
-        {
-            throw new Zend_Exception("Configuration file does not exists (" . $this->config_file . ")");
-        }
-        
+
         // retrieve the configuration from config_file
-        $secret_config = new Zend_Config_Ini($this->config_file, APPLICATION_ENV);
-        
-        // Check if config is an array
-        $configuration_parameters_key = $this->configuration_parameters_key;
-        if(!is_array($secret_config->$configuration_parameters_key->toArray()))
-        {
-            throw new Zend_Exception("Configuration parameters is not an array. '" . $configuration_parameters_key . "' key used.");
-        }
-        
+        $secret_oauth_config = $this->_helper->getConfigurationValues($this->config_file, $this->configuration_parameters_key);
+
         // Initialize consumer object with config array
-		$this->consumer = new Zend_Oauth_Consumer($secret_config->$configuration_parameters_key->toArray());
+		$this->consumer = new Zend_Oauth_Consumer($secret_oauth_config);
     }
 
     /**
