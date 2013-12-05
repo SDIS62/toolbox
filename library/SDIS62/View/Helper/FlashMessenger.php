1dc22527de2984c05224a6abe8dcfe8142717b34
Kévin DUBUC <kevindubuc62@gmail.com>
2013-12-05 10:12:03 +0100

    fix les doublons du FlashMessenger

--- library/SDIS62/View/Helper/FlashMessenger.php
+++ library/SDIS62/View/Helper/FlashMessenger.php
@@ -72,7 +72,7 @@ class SDIS62_View_Helper_FlashMessenger extends Zend_View_Helper_Abstract  imple
         // Current Messages
         $array_currentMessages = $this->flashMessenger->getCurrentMessages();
 
-        return array_unique( array_merge($array_currentMessages, $array_messages) );
+        return $this->arrayUnique(array_merge($array_currentMessages, $array_messages));
     }
 
     /**
@@ -93,5 +93,46 @@ class SDIS62_View_Helper_FlashMessenger extends Zend_View_Helper_Abstract  imple
     public function count()
     {
         return $this->flashMessenger->count();
+    }
+    
+    /** 
+     * Création d'un tableau multidimensionnel sans doublons
+     * 
+     * @param array $array 
+     * @return array 
+     */  
+    function arrayUnique($array, $preserveKeys = false)  
+    {  
+        // Unique Array for return  
+        $arrayRewrite = array();  
+        
+        // Array with the md5 hashes  
+        $arrayHashes = array();  
+        
+        foreach($array as $key => $item)
+        {  
+            // Serialize the current element and create a md5 hash  
+            $hash = md5(serialize($item));  
+            
+            // If the md5 didn't come up yet, add the element to  
+            // to arrayRewrite, otherwise drop it  
+            if (!isset($arrayHashes[$hash]))
+            {  
+                // Save the current element hash  
+                $arrayHashes[$hash] = $hash;  
+                
+                // Add element to the unique Array  
+                if ($preserveKeys)
+                {  
+                    $arrayRewrite[$key] = $item;  
+                }
+                else
+                {  
+                    $arrayRewrite[] = $item;  
+                }  
+            }  
+        }
+        
+        return $arrayRewrite;  
     }
 }
